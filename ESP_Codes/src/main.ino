2f4b19ed1d3bc84ade49cb6c2d8e0f99299e5153
Cesar <czaraugust@gmail.com>
2018-08-20 15:34:43 -0300

    Adding Update OTA

--- ESP_Codes/src/main.ino
+++ ESP_Codes/src/main.ino
@@ -1,6 +1,8 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <ArduinoOTA.h>
 
 
 
@@ -22,47 +24,87 @@ int value = 0;
 void setup_wifi() {
 
   WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
+  WiFi.begin(ssid, password);
+
+  //WiFi.disconnect();
   delay(100);
   // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
-  WiFi.begin(ssid, password);
+  //WiFi.begin(ssid, password);
+
+
   int connRes = WiFi.waitForConnectResult();
   Serial.print ( "connRes: " );
   Serial.println ( connRes );
- /*while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }*/
 
-  //randomSeed(micros());
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  //Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname("station");
+
+    // No authentication by default
+  ArduinoOTA.setPassword("admin");
+
+    // Password can be set with it's md5 value as well
+    // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+    // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+    ArduinoOTA.onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH) {
+        type = "sketch";
+      } else { // U_SPIFFS
+        type = "filesystem";
+      }
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    });
+    ArduinoOTA.onEnd([]() {
+      Serial.println("\nEnd");
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) {
+        Serial.println("Auth Failed");
+      } else if (error == OTA_BEGIN_ERROR) {
+        Serial.println("Begin Failed");
+      } else if (error == OTA_CONNECT_ERROR) {
+        Serial.println("Connect Failed");
+      } else if (error == OTA_RECEIVE_ERROR) {
+        Serial.println("Receive Failed");
+      } else if (error == OTA_END_ERROR) {
+        Serial.println("End Failed");
+      }
+    });
+    ArduinoOTA.begin();
+    Serial.println("Ready");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  // Serial.print("Message arrived [");
-  // Serial.print(topic);
-  // Serial.print("] ");
-  // for (int i = 0; i < length; i++) {
-  //   Serial.print((char)payload[i]);
-  // }
-  // Serial.println();
-  //
-  // // Switch on the LED if an 1 was received as first character
-  // if ((char)payload[0] == '1') {
-  //   digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-  //   // but actually the LED is on; this is because
-  //   // it is acive low on the ESP-01)
-  // } else {
-  //   digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  //}
 
 }
 
@@ -70,6 +112,7 @@ void reconnect() {
 
   while (WiFi.status() != WL_CONNECTED){
     Serial.println("Rec wifi");
+    ESP.restart();
     setup_wifi();
   }
   // Loop until we're reconnected
@@ -113,6 +156,7 @@ void setup() {
 
 void loop() {
 
+ArduinoOTA.handle();
 
 
 
@@ -132,7 +176,12 @@ void loop() {
       if (incomingByte == '@'){
         String values = "\0";
           values += Serial.readString();
-          client.publish(mainTopic, values.c_str());
+
+          values += String(WiFi.RSSI());
+          values += '|';
+
+      client.publish(mainTopic, values.c_str());
+
 
       }
 
